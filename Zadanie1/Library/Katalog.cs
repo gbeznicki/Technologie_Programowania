a840567739a28e9af6abfc6a8163557c80981a88
Gustaw Beźnicki <210140@edu.p.lodz.pl>
2018-03-02 14:05:09 +0100

    Dodano klasy: Katalog, Wykaz, OpisStanu w rozwiązaniu Library

---
+++ Zadanie1/Library/Katalog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+//klasa opisująca katalog książek
+namespace Library
+{
+    class Katalog
+    {
+        //klucz główny
+        public string Isbn
+        {
+            get; set;
+        }
+        public string Title
+        {
+            get; set;
+        }
+        public string Author
+        {
+            get; set;
+        }
+        public int ReleaseYear
+        {
+            get; set;
+        }
+    }
+}
---
+++ Zadanie1/Library/OpisStanu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//klasa opisująca stan książek
+namespace Library
+{
+    class OpisStanu
+    {
+        public string BookDescription
+        {
+            get; set;
+        }
+        public int Quantity
+        {
+            get; set;
+        }
+        public DateTime DateOfPurchase
+        {
+            get; set;
+        }
+
+        public Katalog Catalogue
+        {
+            get; set;
+        }
+    }
+}
