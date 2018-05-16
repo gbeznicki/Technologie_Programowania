20038ccd0b256f367bd03f120c114f8580d538e8
gbeznicki <g.beznicki@gmail.com>
2018-05-16 18:39:28 +0200

    Done

--- Zadanie3/Program/Program.cs
+++ Zadanie3/Program/Program.cs
@@ -21,67 +21,8 @@ namespace Program
                 myProducts.Add(new MyProduct(product));
             }
 
-
-            //// przygotowanie listy MyProduct
-            //List<MyProduct> myProducts = new List<MyProduct>();
-
-            //var allProducts = dc.Product.ToList();
-
-            //foreach (var product in allProducts)
-            //{
-            //    myProducts.Add(new MyProduct(product));
-            //}
-
-            //var myProductsByName = MyTools.GetProductsByVendorName(myProducts, "Australia Bike Retailer");
-            //foreach (var myProduct in myProductsByName)
-            //{
-            //    Console.WriteLine(myProduct.Name);
-            //}
-
-
-
-            //var lista2 = Tools.GetProductsByVendorName("bicycles");
-            //foreach (var itProduct in lista2)
-            //{
-            //    Console.WriteLine(itProduct.Name);
-            //}
-            //Console.WriteLine("-----------------------------------------------");
-
-            ////try
-            ////{
-            ////    var podzielonaLista = lista2.PodzielNaStrony(5, 6);
-            ////    foreach (var itProduct in podzielonaLista)
-            ////    {
-            ////        Console.WriteLine(itProduct.Name);
-            ////    }
-            ////}
-            ////catch (Exception e)
-            ////{
-            ////    Console.WriteLine(e);
-            ////}
-
-
-            //var categories = dc.ProductCategory.Where(c => c.Name.Equals("Bikes")).ToList();
-
-            //var lista3 = Tools.GetTotalStandardCostByCategory(categories[0]);
-            ////Console.WriteLine(lista3);
-            ////foreach (var itProduct in lista)
-            ////{
-            ////    Console.WriteLine(itProduct.Name);
-            ////}
-
-            //var vendorsForGivenProductName = Tools.GetProductVendorByProductName("Crankarm");
-            ////Console.WriteLine(vendorsForGivenProductName);
-
-            //Console.ReadKey();
-
-            var lista = Tools.GetProductByName("Headset");
-
-            //Console.WriteLine(lista.GetProductAndVendorString());
-
             int number = 5;
             var myProducts1 = MyTools.GetNRecentlyReviewedProducts(myProducts, number);
-            var products = Tools.GetNRecentlyReviewedProducts(number);
 
             foreach (var itProduct in myProducts1)
             {
